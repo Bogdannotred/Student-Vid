8b63e02c51aa648d9c70aeb4288a1bc3040cee72
Bogdannotred <96499764+Bogdannotred@users.noreply.github.com>
2025-12-07 21:18:03 +0200

    first

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="bg-black">
+            <div className="container mx-auto px-4 py-2">
+                <h1 className="text-white text-3xl font-bold">My Application</h1>
+            </div>
+        </header>
+    )
+};
+export default Header;
